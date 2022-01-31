a3856504da7c77c72a15c99d1b7f64e258e2ce13
hyemin <bg565@naver.com>
2022-01-31 22:18:20 +0900

    설명 추가

--- 자료구조와 알고리즘/Code/InsertionSort.java
+++ 자료구조와 알고리즘/Code/InsertionSort.java
@@ -14,7 +14,8 @@ class InsertionSort{
                 j--; //key값과 그 다음 왼쪽의 값과 비교
             }
             arr[j+1] = key; //while문에서 마지막 j--을 해주기 때문에 j+1
-
+            //두 번째 인덱스부터 시작하여 한 칸 씩 늘려가면서 왼쪽부터 정렬된다고 생각하면 된다.
+            //첫 번째 패스에서는 0,1 인덱스 값이 서로 정렬되고, 그 다음엔 0,1,2 ... 0,1,2,3 그 다음은 배열 끝까지 정렬되는 것이다.
         }
 
     }
@@ -32,7 +33,5 @@ class InsertionSort{
         print(arr);
         insertionSort(arr);
         print(arr);
-
     }
-
 }
